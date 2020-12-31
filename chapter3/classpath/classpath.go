e57c9c0caba435d16e22ae25c103507dd7ad100c
Your Name <you@example.com>
2020-12-31 11:03:00 +0800

    fix

---
+++ chapter3/classpath/classpath.go
@@ -0,0 +1,92 @@
+package classpath
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+type Classpath struct {
+	bootClasspath Entry
+	extClasspath  Entry
+	userClasspath Entry
+}
+
+func Parse(jreOption, cpOption string) *Classpath {
+
+	cp := &Classpath{}
+	cp.parseBootAndExtClasspath(jreOption)
+	cp.pareseUserClasspath(cpOption)
+
+	return cp
+}
+
+func (self *Classpath) ReadClass(className string) ([]byte, Entry, error) {
+
+	className = className + ".class"
+	if data, entry, err := self.bootClasspath.readClass(className); err == nil {
+		return data, entry, err
+	}
+
+	if data, entry, err := self.extClasspath.readClass(className); err == nil {
+		return data, entry, err
+	}
+
+	return self.userClasspath.readClass(className)
+}
+
+func (self *Classpath) String() string {
+
+	return self.userClasspath.String()
+}
+
+func (self *Classpath) parseBootAndExtClasspath(jreOption string) {
+	jreDir := getJreDir(jreOption)
+	// jre/lib/*
+
+	fmt.Println("get jreDir :", jreDir)
+
+	jreLibPath := filepath.Join(jreDir, "lib", "*")
+
+	fmt.Println("get jreLibPath :", jreLibPath)
+
+	self.bootClasspath = newWildcardEntry(jreLibPath)
+	//jre/lib/ext/*
+	jreExtPath := filepath.Join(jreDir, "lib", "ext", "*")
+
+	fmt.Println("get jreExtPath :", jreLibPath)
+	self.extClasspath = newWildcardEntry(jreExtPath)
+}
+
+func getJreDir(jreOption string) string {
+	if jreOption != "" && exists(jreOption) {
+		return jreOption
+	}
+
+	if exists("./jre") {
+		return "./jre"
+	}
+
+	if jh := os.Getenv("JAVA_HOME"); jh != "" {
+		return filepath.Join(jh, "jre")
+	}
+
+	panic("can not find jre folder !")
+
+}
+
+func exists(path string) bool {
+	if _, err := os.Stat(path); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
+
+func (self *Classpath) pareseUserClasspath(cpOption string) {
+	if cpOption == "" {
+		cpOption = "."
+	}
+	self.userClasspath = newEntry(cpOption)
+}
